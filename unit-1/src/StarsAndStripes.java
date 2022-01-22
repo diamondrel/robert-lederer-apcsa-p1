65c4befe7bdb4f5c18eeb7571513704b890ca5a3
diamondrel <49543759+diamondrel@users.noreply.github.com>
2022-01-21 18:44:37 -0800

    Started Stars And Stripes

---
+++ unit-1/src/StarsAndStripes.java
@@ -0,0 +1,47 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+	   out.println("____________________");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   out.println("");
+	   out.println("");
+   }
+   
+   public void printASmallBox()
+   {	
+	   out.println("_____");
+	   out.println("|\t|");
+	   out.println("|\t|");
+	   out.println("|\t|");
+	   out.println("_____");
+   }
+ 
+   public void printABigBox()
+   { 	
+   }   
+}
