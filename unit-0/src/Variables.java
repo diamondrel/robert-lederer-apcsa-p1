15173765dbc85a609b61c27c1b28b5fdc90d1732
diamondrel <49543759+diamondrel@users.noreply.github.com>
2022-01-21 18:01:16 -0800

    Finished unit 0

--- unit-0/src/Variables.java
+++ unit-0/src/Variables.java
@@ -18,8 +18,15 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
-
-
+		short shortOne = -5004;
+		int intOne = 321144;
+		long longOne = 29423994;
+		float floatOne = 981;
+		double doubleOne = 9.81;
+		char charOne = 'A';
+		String stringOne = "hello world";
+		boolean booleanOne = true;
+		
 		//decimal variables
 
 
@@ -31,21 +38,28 @@ public class Variables
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
-		System.out.println("*                               *");
+		System.out.println("*Robert Lederer         01/21/22*");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-
-
-
-
-
-
-
-
-
-
-
-
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*          real types           *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*          other types          *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+booleanOne+"\t\t*");
+		System.out.println("*stringOne = "+stringOne+"\t*");
+		System.out.println("/////////////////////////////////");
+		
 	}
 }
