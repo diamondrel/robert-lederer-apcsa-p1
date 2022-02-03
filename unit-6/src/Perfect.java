6d70c3892be6cd7ed152f1c6fc857261ba19568a
diamondrel <49543759+diamondrel@users.noreply.github.com>
2022-02-03 09:07:35 -0800

    Worked on PerfectSum

--- unit-6/src/Perfect.java
+++ unit-6/src/Perfect.java
@@ -7,16 +7,38 @@ import static java.lang.System.*;
 public class Perfect
 {
    private int number;
+   private String s;
 
-	//add constructors
-
-	//add a set method
+	public Perfect() {
+		
+	}
+	
+	public Perfect(int num) {
+		setNum(num);
+	}
+	
+	public void setNum(int n) {
+		number = n;
+	}
 
 	public boolean isPerfect()
 	{
+		for (int i=0;i<Math.sqrt(number);i++) {
+			if(i!=0&&number%i == 0&&((number/i)+i)==number) {
+				return true;
+			}
+		}
 		return false;
 	}
 
-	//add a toString	
+	public String toString() {
+		if (isPerfect()) {
+			s = " is perfect";
+		}
+		else {
+			s = " is not perfect";
+		}
+		return number + s;
+	}
 	
 }
