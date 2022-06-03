55b16d173e95d65c221e441bfecbea8624ce94df
diamondrel <49543759+diamondrel@users.noreply.github.com>
2022-06-03 10:24:16 -0700

    Update Perceptron.java

--- unit-18/src/Perceptron.java
+++ unit-18/src/Perceptron.java
@@ -1,14 +1,26 @@
+import java.util.Arrays;
+
 public class Perceptron{
 	int threshold;
-	int rate;
+	double rate;
 	int[] weights;
 	int bias;
-	public Perceptron(int nInputs, int thresholdSet, int learningRate) {
+	public Perceptron(int nInputs) {
+		threshold=100;
+		rate=0.01;
+		weights=new int[nInputs];
+		bias=0;
+		System.out.println("Initial weights: "+ weights);
+		System.out.println("Initial bias: "+ bias);
+		System.out.println("Threshold: "+ threshold);
+		System.out.println("Learning rate: "+ rate+ "\n");
+	}
+	public Perceptron(int nInputs, int thresholdSet, double learningRate) {
 		threshold=thresholdSet;
 		rate=learningRate;
 		weights=new int[nInputs];
 		bias=0;
-		System.out.println("Initial weights: "+ weights);
+		System.out.println("Initial weights: "+ Arrays.toString(weights));
 		System.out.println("Initial bias: "+ bias);
 		System.out.println("Threshold: "+ threshold);
 		System.out.println("Learning rate: "+ rate+ "\n");
@@ -31,9 +43,10 @@ public class Perceptron{
 		int yhat=0;
 		int error=0;
 		for(int i=0;i<threshold;i++) {
-			System.out.println("Iteration "+i);
-			for(int j=0;j<inputs.length;j++) {
-				System.out.println("Current input: "+inputs[j]+" | Current weight: "+weights[j]+" | Current bias: "+bias);
+			System.out.println("\nIteration "+i);
+			for(int j=0;j<inputs[0].length;j++) {
+				System.out.println("\nJteration "+j);
+				System.out.println("Current input: "+Arrays.toString(inputs[j])+" | Current weight: "+weights[j]+" | Current bias: "+bias);
 				yhat=predict(inputs[j]);
 				System.out.println("Activation(yhat): "+yhat);
 				error=labels[j]-yhat;
@@ -42,9 +55,38 @@ public class Perceptron{
 					weights[j]+=(rate*error*inputs[j][k]);
 				}
 				bias+=rate*error;
-				System.out.println("Updated Weights: "+weights);
-				System.out.println("Updated Weights: "+weights);
+				System.out.println("Updated Weights: "+Arrays.toString(weights));
+				System.out.println("Updated Biases: "+bias);
 			}
 		}
+	}
+	public static void main(String[] args) {
+		int[][] inputMatrix = new int[4][2];
+		int[] labels={1,0,0,0};
+		
+		String gate="AND";
+		
+		if(gate.equals("OR"))
+			labels = new int[]{1,1,1,0};
+		else if(gate.equals("NAND"))
+			labels = new int[]{0,1,1,1};
+		else if(gate.equals("XOR")) 
+			labels = new int[]{0,1,1,0};
+		
+		inputMatrix[0]= new int[] {1,1};
+		inputMatrix[1]=new int[] {1,0};
+		inputMatrix[2]= new int[] {0,1};
+		inputMatrix[3]=new int[] {0,0};
+		
+		Perceptron perceptron = new Perceptron(2,10,1);
+		
+		perceptron.train(inputMatrix, labels);
+		
+		int a=0;
+		int b=1;
+		System.out.println("\nResults\n");
+		System.out.println("Inputs: "+a+", "+b);
+		int output=perceptron.predict(new int[] {a,b});
+		System.out.println("Output: "+output);
 	}
 }
